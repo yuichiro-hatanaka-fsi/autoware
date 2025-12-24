f06533f00a50d3873ced394b3aea2faa2878b805
yuichiro-hatanaka-fsi <hatanayu@fsi.co.jp>
2025-12-24 11:25:05 +0900

    Update run.sh
    公開鍵の競合が起こっているので、04-02でコピーした.webauto-ciディレクトリにあるautoware-setupのrun.shファイルの、5・6行目の記述をコメントアウト
    
    削除→	sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
    削除→	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo "$UBUNTU_CODENAME") main" | sudo tee /etc/apt/sources.list.d/ros2.list >/dev/null

--- .webauto-ci/main/autoware-setup/run.sh
+++ .webauto-ci/main/autoware-setup/run.sh
@@ -2,8 +2,9 @@
 
 : "${WEBAUTO_CI_GITHUB_TOKEN:?is not set}"
 
-sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo "$UBUNTU_CODENAME") main" | sudo tee /etc/apt/sources.list.d/ros2.list >/dev/null
+#公開鍵の競合が起こっているのでコメントアウト
+#sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+#echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo "$UBUNTU_CODENAME") main" | sudo tee /etc/apt/sources.list.d/ros2.list >/dev/null
 sudo -E apt-get -y update
 sudo -E apt-get -y install usbutils # For kvaser
 
